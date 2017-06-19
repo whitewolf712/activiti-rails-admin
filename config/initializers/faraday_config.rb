3cdefdd3ac2ee05a85a32cc79124846f3864ba09
Eugeny Ponomarenko <ponomarenko@ntk.novotelecom.ru>
2017-06-19 15:51:49 +0700

    Fix faraday options

--- config/initializers/faraday_config.rb
+++ config/initializers/faraday_config.rb
@@ -1,5 +1,7 @@
 ActiveRestClient::Base.faraday_config do |faraday|
-  faraday.adapter(:net_http)
+  faraday.adapter(:patron)
   faraday.options.timeout       = 300
   faraday.headers['User-Agent'] = 'ActivitiRailsAdmin/1.0.0'
+  faraday.headers['Connection'] = 'Keep-Alive'
+  faraday.headers['Accept'] = 'application/json'
 end
